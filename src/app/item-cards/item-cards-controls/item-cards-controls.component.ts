e1818387ffc99ef30277981344f7a277accfed30
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2023-03-10 18:12:43 +0100

    feat: export to json file

--- src/app/item-cards/item-cards-controls/item-cards-controls.component.ts
+++ src/app/item-cards/item-cards-controls/item-cards-controls.component.ts
@@ -1,12 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { SegmentCustomEvent } from '@ionic/angular';
 
+import { Card } from '../item-cards.type';
+
 @Component({
     selector: 'app-item-cards-controls',
     templateUrl: './item-cards-controls.component.html',
 })
 export class ItemCardsControlsComponent {
     @Input() preview = false;
+    @Input() cards: Card[] = [];
     @Output() togglePreviewEvent = new EventEmitter<boolean>();
 
     togglePreview = (segmentCustomEvent: SegmentCustomEvent) =>
--- src/app/shared/json-exporter/json-exporter.component.ts
+++ src/app/shared/json-exporter/json-exporter.component.ts
@@ -6,4 +6,30 @@ import { Component, Input } from '@angular/core';
 })
 export class JsonExporterComponent {
     @Input() label = 'Export JSON';
+    @Input() json = {};
+
+    export = () => {
+        const dataStr =
+            'data:text/json;charset=utf-8,' +
+            encodeURIComponent(JSON.stringify(this.json));
+        const downloadAnchorNode = document.createElement('a');
+        downloadAnchorNode.setAttribute('href', dataStr);
+        downloadAnchorNode.setAttribute('download', this.getExportName());
+        document.body.appendChild(downloadAnchorNode); // required for firefox
+        downloadAnchorNode.click();
+        downloadAnchorNode.remove();
+    };
+
+    getExportName = () => {
+        const prefix = this.label
+            .split(' ')
+            .map((x) => x.toLowerCase())
+            .join('_');
+        const date = new Date();
+        const timestamp = `${date.getFullYear()}_${
+            date.getMonth() + 1
+        }_${date.getDate()}_${date.getHours()}_${date.getMinutes()}_${date.getSeconds()}`;
+        const extension = 'json';
+        return `${prefix}_${timestamp}.${extension}`;
+    };
 }
