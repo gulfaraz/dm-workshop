b281a01d88d4e7cb0c2d1fcb9ac66b2c2a9c7fbd
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2023-04-18 00:44:25 +0200

    fix: set grid gap to 0.5mm

--- src/app/combat-tokens/combat-tokens.service.ts
+++ src/app/combat-tokens/combat-tokens.service.ts
@@ -3,7 +3,7 @@ import { Token, sizes } from './combat-tokens.type';
 
 export class CombatTokensService extends SheetService<Token> {
     rows = 16;
-    columns = 23;
+    columns = 22;
 
     getSize = (token: Token) => sizes[token.size as keyof typeof sizes];
 }
