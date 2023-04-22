394f52c0d3891fbf89c8f6674e16416e9a145275
Gulfaraz Rahman <gulfarazyasin@gmail.com>
2023-04-22 18:11:46 +0200

    feat: firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getAnalytics, provideAnalytics } from '@angular/fire/analytics';
 
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DMWorkshopComponent } from './dm-workshop.component';
@@ -11,6 +14,8 @@ import { DMWorkshopComponent } from './dm-workshop.component';
         BrowserModule,
         IonicModule.forRoot({ mode: 'ios' }),
         AppRoutingModule,
+        provideFirebaseApp(() => initializeApp(environment.firebase)),
+        provideAnalytics(() => getAnalytics()),
     ],
     declarations: [AppComponent, DMWorkshopComponent],
     bootstrap: [AppComponent],
--- src/app/dm-workshop.component.ts
+++ src/app/dm-workshop.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { inject } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Analytics, logEvent } from '@angular/fire/analytics';
 
 @Component({
     selector: 'app-dm-workshop',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class DMWorkshopComponent {
     paymentLink = 'https://buy.stripe.com/5kAcMMbGXc0NexW144';
+
+    private analytics: Analytics = inject(Analytics);
+
+    constructor(router: Router) {
+        router.events.subscribe((event) => {
+            if (event instanceof NavigationEnd) {
+                logEvent<string>(this.analytics, 'screen_view', {
+                    firebase_screen: event.urlAfterRedirects,
+                });
+            }
+        });
+    }
 }
